9c7ca2d3ff0ad27729bdcb0ff7f8baff0181f17d
esma <e272692@sbl.ch>
2023-03-17 10:10:02 +0100

    spieler 2

--- src/config.js
+++ src/config.js
@@ -6,10 +6,7 @@ const config = {
     "KeyA": function() { Game.player.move("left")},
     "KeyS": function() { Game.player.move("down")},
     "KeyD": function() { Game.player.move("right")},
-    "ArrowUp": function() { Game.player2.move("up")},
-    "ArrowLeft": function() { Game.player2.move("left")},
-    "ArrowDown": function() { Game.player2.move("down")},
-    "ArrowRight": function() { Game.player2.move("right")},
+    
   }
 }
 
